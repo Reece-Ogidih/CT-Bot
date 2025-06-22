c7daccac46f3de577036f4eb00e1abce602aa055
Reece Ogidih <reeceogidih@gmail.com>
2025-06-22 19:14:09 +0100

    Tidied up some comments

--- cmd/HistFetch/main.go
+++ cmd/HistFetch/main.go
@@ -7,6 +7,8 @@ import (
 	histdata "github.com/Reece-Ogidih/CT-Bot/HistoricalData"
 )
 
+// Time-wise we start with 1 year of data since it is short term day trading bot so should be sufficient, if model doesnt seem robust can extend to 2 years.
+
 func main() {
 	from := time.Date(2024, 6, 22, 0, 0, 0, 0, time.UTC)
 	to := time.Date(2025, 6, 22, 0, 0, 0, 0, time.UTC)
